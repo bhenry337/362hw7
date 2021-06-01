aa7667914de12b397249b72a46d2c02abee691ed
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-05-31 21:16:53 -0700

    Added 2nd test to test divisble by 100

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,4 +5,6 @@ class TestCase(unittest.TestCase):
 	def test_div4(self):
 		self.assertEqual(leapyear.leapyear(2004), True)
 		pass
-		
+	def test_div100(self):
+		self.assertEqual(leapyear.leapyear(2100), False)
+		pass
